cfd746588e2ba82127653ba1ac8b5caf0c57e386
Shikhar1401 <shikharagarwal0717@gmail.com>
2022-11-07 14:25:23 +0530

    Add:Setup Files and Common files

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,15 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+
+    sassOptions: {
+      extension: 'scss'
+    },
+    'ember-bootstrap': {
+      'bootstrapVersion': 4,
+      'importBootstrapFont': false,
+      'importBootstrapCSS': true
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
