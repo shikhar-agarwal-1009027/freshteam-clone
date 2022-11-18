026c1ee879a6cc2c88c4793b9540dcf84a0391cb
Shikhar1401 <shikharagarwal0717@gmail.com>
2022-11-18 14:21:33 +0530

    Users list with PR review Comments

--- tests/integration/components/user-avatar-test.js
+++ tests/integration/components/user-avatar-test.js
@@ -6,21 +6,40 @@ import hbs from 'htmlbars-inline-precompile';
 module('Integration | Component | user-avatar', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  hooks.beforeEach(function () {
+    this.store = this.owner.lookup('service:store');
+    this.user = this.store.createRecord('user', {
 
-    await render(hbs`{{user-avatar}}`);
+      img_url: 'fake.png',
+      first_name: 'temp-first_name',
+      last_name: 'temp-last_name',
+      email: 'temp-email@gmail.com',
+      team: 'temp-team',
+      joiningDate: 'temp-date'
+    });
+    this.user1 = this.store.createRecord('user', {
 
-    assert.equal(this.element.textContent.trim(), '');
+      first_name: 'temp-first_name',
+      last_name: 'temp-last_name',
+      email: 'temp-email@gmail.com',
+      team: 'temp-team',
+      joiningDate: 'temp-date'
+    });
+  });
+  
 
-    // Template block usage:
-    await render(hbs`
-      {{#user-avatar}}
-        template block text
-      {{/user-avatar}}
-    `);
+  test('user avatar renders image when img_url is present', async function(assert) {
+    await render(hbs`{{user-avatar user=this.user}}`);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom('[data-test-id="user-avatar"]').exist('avatar image does exist');
+    assert.dom('[data-test-id="no-user-avatar"]').doesNotExist('avatar image does not exist');
+    assert.dom('[data-test-img-id="user-avatar-img]').hasAttribute('src', user.img_url)
+  });
+  test('user avatar renders image when img_url is present', async function(assert) {
+    await render(hbs`{{user-avatar user=this.user1}}`);
+    
+    assert.dom('[data-test-id="user-avatar"]').doesNotExist('avatar image does exist');
+    assert.dom('[data-test-id="no-user-avatar"]').exist('avatar image does not exist');
+    assert.dom('[data-test-id="no-user-avatar"]').hasText('T');
   });
 });
--- tests/integration/components/user-card-test.js
+++ tests/integration/components/user-card-test.js
@@ -5,22 +5,27 @@ import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | user-card', function(hooks) {
   setupRenderingTest(hooks);
+  hooks.beforeEach(function () {
+    this.store = this.owner.lookup('service:store');
+    this.user = this.store.createRecord('user', {
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+      img_url: 'fake.png',
+      first_name: 'temp-first_name',
+      last_name: 'temp-last_name',
+      email: 'temp-email@gmail.com',
+      team: 'temp-team',
+      joiningDate: 'temp-date'
+    });
+  });
 
-    await render(hbs`{{user-card}}`);
+  test('user card is having a full name', async function(assert) {
+    await render(hbs`{{user-card user=this.user}}`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.dom('[data-test-id="user-name"]').hasText('temp-first_name temp-last_name')
+  });
+  test('user card is having an email', async function(assert) {
+    await render(hbs`{{user-card user=this.user}}`);
 
-    // Template block usage:
-    await render(hbs`
-      {{#user-card}}
-        template block text
-      {{/user-card}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom('[data-test-id="user-name"]').hasText('temp-email@gmail.com')
   });
 });
