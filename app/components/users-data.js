f4b17f552619afd674de6fed88b0004e3fe2fa65
Shikhar1401 <shikharagarwal0717@gmail.com>
2022-11-30 17:45:19 +0530

    Done: usage of yield

--- app/components/users-data.js
+++ app/components/users-data.js
@@ -23,48 +23,6 @@ export default Component.extend({
         let temp = sortValues.map((el) => el.split('_').join(' '));
         return temp;
     }),
-
-    filteredUsers: computed('selectedTeam', function () {
-        if (this.selectedTeam === 'All Employees') return this.model.filter((el)=>el);
-        else return this.model.filter((el) => el.team === this.selectedTeam);
-    }),
-
-    sortedResults: computed('filteredUsers.[]', 'selectedSort', 'selectedSortOrder', function () {
-        let filterUsers = get(this, 'filteredUsers');
-        let sortType = get(this, 'selectedSort');
-        let x = sortType.split(' ').join('_').toLowerCase();
-        if (sortType === 'joining date') {
-            x = 'joiningDate';
-        }
-        let sortOrderType = get(this, 'selectedSortOrder');
-    
-        if (sortOrderType === 'Ascending' || "") {
-            filterUsers.sort(function (a, b) {
-                return (a[x]<b[x]) ? -1 : 1
-            })
-        } else {
-            filterUsers.sort(function (a, b) {
-                return (a[x]>b[x] ? -1: 1)
-            })
-        }
-        return filterUsers;
-    }),
-    searchResult: computed('sortedResults.[]', 'inputValue', function () {
-        let searchInputValue = this.inputValue;
-        let sortedUsers = get(this, 'sortedResults');
-        let temp = [];
-
-        if (searchInputValue !== '' || searchInputValue !== null) {
-            temp = sortedUsers.filter((_el) => {
-                if (_el['first_name'].toLowerCase().includes(searchInputValue.toLowerCase()) > 0) return _el;
-            })
-            return temp
-        }
-
-        return sortedUsers;
-
-    }),
-    
     actions: {
         selectFilterOption(value) {
             if (this.teams.indexOf(value) !== -1) this.set('selectedTeam', value);
